6cd67980529d932381a1b1bbc498c0a1c11ccb84
abhay gupta <akg568794@gmail.com>
2024-06-23 14:03:54 +0530

    Aligned profile section to the right ( 1:24:34)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,26 @@
 import CurrentUSer from "./current-user"
+import { Layout,Space } from "antd"
 
 const Header = () => {
+  const headerStyles:React.CSSProperties = {
+    background: '#fff',
+    display:'flex',
+    justifyContent: 'flex-end',
+    alignItems:'center',
+    padding:' 0 24px',
+    position:'sticky',
+    top:0,
+    zIndex:999,
+    boxShadow:'0 1px 4px rgba(0, 0, 0, 0.08)',
+    borderBottom:'1px solid #f0f0f0'
+
+  }
   return (
-    <div>
-        <CurrentUSer/>
-    </div>
+    <Layout.Header style={headerStyles}>
+      <Space  align="center" size="middle">
+        <CurrentUSer/> 
+      </Space>
+    </Layout.Header>
   )
 }
 
