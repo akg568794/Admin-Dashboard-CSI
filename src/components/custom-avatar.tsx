af9ab2b5d57ef9723c5554fa5f6eb63f84e3d9be
abhay gupta <akg568794@gmail.com>
2024-06-23 13:55:18 +0530

    completed the header component (1:22:00)

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,12 +1,25 @@
-import { Avatar as AntdAvatar } from "antd"
+import {getNameInitials} from "@/utilities"
+import { Avatar as AntdAvatar,AvatarProps } from "antd"
+type Props=AvatarProps & {
+    name?:string;
+}
 
-const CustomAvatar = () => {
+const CustomAvatar = ({name,style,...rest}:Props) => {
   return (
     <AntdAvatar 
-    alt={'Ak'} 
+    alt={name} 
     size="small"
-    style={{backgroundColor:"#87d068"}}>
-        Ak
+    style={{
+        backgroundColor:"#87d068",
+        display:'flex',
+        alignItems:'center',
+        border:'none',
+        ...style
+        }}
+        {...rest}
+        >
+        {getNameInitials(name || " ")}
+        {name}
     </AntdAvatar>
   )
 }
